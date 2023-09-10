a02694a271a9b2e1e1dfa88c90fa16a33573b36c
Dmitri Kozhukhov <dmitrikozhukhov@gmail.com>
2023-09-11 02:34:02 +0300

    Task13

---
+++ HomeWork2/Task13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+// 123456789 -> 3
+int number = Convert.ToInt32(Console.ReadLine()!);
+if ((number / 100) < 1)
+    System.Console.WriteLine("Третьей цифры нет");
+else
+{
+    int remain = number;
+    int result = 0;
+    while (remain > 100)
+    {
+        result = remain % 10;
+        remain = remain / 10;
+    }
+    System.Console.WriteLine(result);
+}
