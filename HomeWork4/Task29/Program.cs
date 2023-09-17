8237aeb7da1f358762b38b0875ed6911ccd37153
Dmitri Kozhukhov <dmitrikozhukhov@gmail.com>
2023-09-17 21:27:52 +0300

    Task 29

--- HomeWork4/Task29/Program.cs
+++ HomeWork4/Task29/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+int[] CreateArray(int minVal, int maxVal, int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(minVal, maxVal + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if  (i == array.Length - 1)
+           System.Console.Write(array[i] + " ");
+        else
+            System.Console.Write(array[i] + ", ");
+    }
+    System.Console.WriteLine();
+}
+int length = 8;
+System.Console.WriteLine("Введите нижнюю границу диапазона чисел");
+int minVal = Convert.ToInt32(Console.ReadLine()!);
+System.Console.WriteLine("Введите верхнюю границу диапазона чисел");
+int maxVal = Convert.ToInt32(Console.ReadLine()!);
+PrintArray(CreateArray(minVal, maxVal, length));
